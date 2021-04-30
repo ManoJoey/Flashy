289b96ca50e254649d9d48077e84d98793466da0
Me255 <82172103+Me255@users.noreply.github.com>
2021-04-30 17:26:23 +0200

    quick save to import file

--- Flashy_main.py
+++ Flashy_main.py
@@ -8,7 +8,10 @@ homecount = 0
 
 root = tk.Tk()
 root.title("Flashy")
-root.geometry("300x300")
+w = root.winfo_screenwidth()
+h = root.winfo_screenheight()
+geometry = ("%dx%d" % (w,h))
+root.geometry(geometry)
 
 frame_home = tk.Frame(root)
 frame_create = tk.Frame(root)
@@ -30,23 +33,23 @@ def home():
     clear_screen()
     frame_home.grid(row=0, column=0)
     if homecount < 1:
-        welcome_label = tk.Label(frame_home, text="Welcome!", font=("Helvetica", 25))
-        welcome_label.pack(padx=70, pady=1)
-        button_create = tk.Button(frame_home, text="Create new study set", width=20, height=3, command=create_set)
-        button_create.pack()
+        welcome_label = tk.Label(frame_home, text="Welcome!", font=("Helvetica", 50))
+        welcome_label.grid(row=0, column=1, columnspan=3, pady=5)
+        button_create = tk.Button(frame_home, text="Create new study set", width=18, height=5, borderwidth=5, font=("Helvetica", 20), command=create_set)
+        button_create.grid(row=1, column=1)
         homecount += 1
 
 
 #all the menu options
 menu = tk.Menu(root)
 root.config(menu=menu)
-options_menu = tk.Menu(menu, tearoff=False)
-menu.add_cascade(label="options", menu=options_menu)
-options_menu.add_command(label="Home", command=home)
-options_menu.add_command(label="Create set", command=create_set)
-options_menu.add_command(label="Exit", command=root.quit)
+menu.add_command(label="Home", command=home)
+menu.add_command(label="Create set", command=create_set)
+menu.add_command(label="Exit", command=root.quit)
 
 
 home()
+
+root.grid_columnconfigure(0, weight=1)
 
 root.mainloop()
