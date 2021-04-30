404968dc370a9a226369cd592789d73a57d26527
Me255 <82172103+Me255@users.noreply.github.com>
2021-04-30 19:39:14 +0200

    created date

--- Flashy_main.py
+++ Flashy_main.py
@@ -1,8 +1,7 @@
 #Joey mocht je images willen gebruiken dan moet je command prompt opstarten en "pip install Pillow" zeggen, dan doet hij het
-#Joey mocht je images willen gebruiken dan moet je command prompt opstarten en "pip install Pillow" zeggen, dan doet hij het
-#Joey mocht je images willen gebruiken dan moet je command prompt opstarten en "pip install Pillow" zeggen, dan doet hij het
 import tkinter as tk
 from PIL import ImageTk,Image
+from datetime import date
 
 root = tk.Tk()
 root.title("Flashy")
@@ -26,6 +25,7 @@ def create_set():
     frame_create.grid(row=0, column=0)
 
 def study_set():
+    #get button with changing title and definition
     pass
 
 
@@ -33,13 +33,27 @@ def home():
     #creates the home screen
     clear_screen()
     frame_home.grid(row=0, column=0)
+    whitespace = tk.Label(frame_home, text="", bg="black", height=3)
+    whitespace.grid(row=0, column=1)
+
     welcome_label = tk.Label(frame_home, text="Welcome!", font=("Helvetica", 50), fg="white", bg="black")
-    welcome_label.grid(row=0, column=1, columnspan=3, pady=5)
-    button_create = tk.Button(frame_home, text="Create new study set", width=18, height=5, borderwidth=0, font=("Helvetica", 20), command=create_set)
-    button_create.grid(row=1, column=1)
-    start_studying = tk.Button(frame_home, text="Study a set", width=18, height=5, borderwidth=0, font=("Helvetica", 20), command=study_set)
-    start_studying.grid(row=2, column=1, pady=2)
- 
+    welcome_label.grid(row=1, column=1, columnspan=3, pady=20)
+
+    today = str(date.today().strftime("%d-%b-%Y"))
+    today_date = tk.Label(frame_home, text=today, bg="black", fg="white", font=("Helvetica", 30))
+    today_date.grid(row=2, column=1)
+
+    whitespace.grid(row=3, column=1)
+
+    button_create = tk.Button(frame_home, text="Create new study set", width=18, height=4, borderwidth=0, font=("Helvetica", 20), command=create_set)
+    button_create.grid(row=4, column=1)
+
+    start_studying = tk.Button(frame_home, text="Study a set", width=18, height=4, borderwidth=0, font=("Helvetica", 20), command=study_set)
+    start_studying.grid(row=5, column=1, pady=2)
+
+    exit_button = tk.Button(frame_home, text="Exit", width=26, height=3, borderwidth=0, font=("Helvetica", 15), command=root.quit)
+    exit_button.grid(row=6, column=1, pady=100)
+
 
 
 #all the menu options
