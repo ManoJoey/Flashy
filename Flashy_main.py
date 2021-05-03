60cff5c63feb40cdcdda19419389c01c022d970a
Me255 <82172103+Me255@users.noreply.github.com>
2021-05-03 11:42:03 +0200

    fuck dit ik ga straks verder

--- Flashy_main.py
+++ Flashy_main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from PIL import ImageTk,Image
 from datetime import date
 import pygame
@@ -10,7 +11,6 @@ y = 2
 VisitCreateCount = 0
 write_list = []
 read_list = []
-screenheightcount = 50
 list_entries = []
 list_entries_solid = []
 title_set = ""
@@ -52,7 +52,6 @@ def writefile(file_name):
     file_delete.close()
 
 
-
 root = tk.Tk()
 root.title("Flashy")
 root.iconbitmap("F.ico")
@@ -65,6 +64,11 @@ frame_create = tk.Frame(root, bg="black")
 frame_study = tk.Frame(root, bg="black")
 frame_image_side = tk.Frame(root, bg="black", width=750, height=996, borderwidth=0)
 frame_image_side2 = tk.Frame(root, bg="black", width=750, height=996, borderwidth=0)
+title_frame = tk.Frame(root, bg="black")
+create_canvas = tk.Canvas(frame_create, bg="black")
+second_frame = tk.Frame(create_canvas, bg="black")
+frame_buttons_create = tk.Frame(root, bg="black")
+scrollbar = ttk.Scrollbar(frame_create, orient=tk.VERTICAL, command=create_canvas.yview)
 
 def quit_flashy():
     pygame.mixer.music.load("Minecraft hurt.mp3")
@@ -78,6 +82,10 @@ def clear_screen():
     frame_study.grid_forget()
     frame_image_side.grid_forget()
     frame_image_side2.grid_forget()
+    title_frame.pack_forget()
+    create_canvas.pack_forget()
+    second_frame.pack_forget()
+    frame_buttons_create.pack_forget()
 
 
 def done_creating():
@@ -110,47 +118,23 @@ def done_creating():
 
 
 def next_page():
-    global screenheightcount
-    clear_screen()
-    for entry in list_entries:
-        list_entries_solid.append(entry)
-        if list_entries_solid.count(entry) == 2:
-            list_entries_solid.pop(-1)
-        entry.destroy()
-    screenheightcount = 0
-
-    next_page_term = tk.Entry(frame_create, width=75, borderwidth=0, bg="#4c8151")
-    next_page_definition = tk.Entry(frame_create, width=75, borderwidth=0, bg="#4c8151")
-    next_page_term.grid(row=1, column=0, padx=20)
-    next_page_definition.grid(row=1, column=1, padx=85, pady=25)
-
-    list_entries.append(next_page_term)
-    list_entries.append(next_page_definition)
-
-    frame_create.grid(row=0, column=0)
+    pass
 
 def add_term():
-    global y, padcount, screenheightcount
-    pygame.mixer.music.load("Swipe.mp3")
-    pygame.mixer.music.play(loops=0)
+    global y, padcount
+    #pygame.mixer.music.load("Swipe.mp3")
+    #pygame.mixer.music.play(loops=0)
 
-    new_entry_term = tk.Entry(frame_create, width=75, borderwidth=0, bg="#4c8151")
+    new_entry_term = tk.Entry(second_frame, width=75, borderwidth=0, bg="#4c8151")
     new_entry_term.grid(row=y, column=0, pady=25, padx=20)
 
-    new_entry_definition = tk.Entry(frame_create, width=75, borderwidth=0, bg="#4c8151")
+    new_entry_definition = tk.Entry(second_frame, width=75, borderwidth=0, bg="#4c8151")
     new_entry_definition.grid(row=y, column=1, pady=25, padx=85)
 
     list_entries.append(new_entry_term)
     list_entries.append(new_entry_definition)
 
-    create_set_done.grid_configure(row=y+2)
-    new_term.grid_configure(row=y+2)
-
     y += 1
-    screenheightcount += 78
-
-    if screenheightcount > root.winfo_screenheight():
-        next_page()
 
 
 def create_set():
@@ -166,36 +150,41 @@ def create_set():
 
     clear_screen()
 
-    frame_create.grid(row=0, column=0)
-    
-    title_label = tk.Label(frame_create, text="Title:", bg="black", fg="#4c8151", font=("Helvetica", 20))
-    title_label.grid(????)
+    title_frame.pack(anchor="w")
+    frame_create.pack(fill=tk.BOTH, expand=1)
 
-    title_entry = tk.Entry(frame_create, width=? , borderwidth=3, bg="#4c8151")
-    title_entry.grid(????)
+    create_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
-    term_label = tk.Label(frame_create, text="Term:", bg="black", fg="#4c8151", font=("Helvetica", 20))
-    term_label.grid(row=0, column=0, stick=tk.W, padx=20, pady=10)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-    definition_label = tk.Label(frame_create, text="Definition:", bg="black", fg="#4c8151", font=("Helvetica", 20))
-    definition_label.grid(row=0, column=1, stick=tk.W, padx=80, pady=10)
+    create_canvas.configure(yscrollcommand=scrollbar.set)
+    create_canvas.bind("<Configure>", lambda event: create_canvas.configure(scrollregion=create_canvas.bbox("all")))
 
-    term_entry = tk.Entry(frame_create, width=75, borderwidth=0, bg="#4c8151")
-    term_entry.grid(row=1, column=0, padx=20)
-    
-    definition_entry = tk.Entry(frame_create, width=75, borderwidth=0, bg="#4c8151")
-    definition_entry.grid(row=1, column=1, padx=85, pady=25)
+    create_canvas.create_window((0,0), window=second_frame, anchor="nw")
 
-    list_entries.append(term_entry)
-    list_entries.append(definition_entry)
 
-    new_term = tk.Button(frame_create, text="Add term", command=add_term, width=10, height=2, bg="#4c8151", borderwidth=0, font=("Helvetica", 15))
-    new_term.grid(row=2, column=0, sticky=tk.W, padx=20, pady=25)
+    frame_buttons_create.pack(anchor="w")
+
+
+    title_label = tk.Label(title_frame, text="Title:", bg="black", fg="#4c8151", font=("Helvetica", 20))
+    title_label.grid(row=0, column=0, stick=tk.W, padx=20, pady=5)
+
+    title_entry = tk.Entry(title_frame, width=50, borderwidth=0, bg="#4c8151")
+    title_entry.grid(row=0, column=0, padx=100)
+
+    term_label = tk.Label(title_frame, text="Term:", bg="black", fg="#4c8151", font=("Helvetica", 20))
+    term_label.grid(row=1, column=0, stick=tk.W, padx=20, pady=10)
+
+    definition_label = tk.Label(title_frame, text="Definition:", bg="black", fg="#4c8151", font=("Helvetica", 20))
+    definition_label.grid(row=1, column=1, stick=tk.W, padx=80, pady=10)
+
+
+    new_term = tk.Button(frame_buttons_create, text="Add term", command=add_term, width=10, height=2, bg="#4c8151", borderwidth=0, font=("Helvetica", 15))
+    new_term.grid(row=3, column=0, sticky="w", padx=20, pady=25)
     root.bind("<Return>", lambda event: add_term())
 
-
-    create_set_done = tk.Button(frame_create, text="Done", command=done_creating, width=10, height=2, bg="#4c8151", borderwidth=0, font=("Helvetica", 15))
-    create_set_done.grid(row=2, column=1, sticky=tk.E, padx=88)
+    create_set_done = tk.Button(frame_buttons_create, text="Done", command=done_creating, width=10, height=2, bg="#4c8151", borderwidth=0, font=("Helvetica", 15))
+    create_set_done.grid(row=3, column=1, sticky="e", padx=1630)
 
     VisitCreateCount += 1
 
